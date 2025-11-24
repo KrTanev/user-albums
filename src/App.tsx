c937713208b0071c6f5dceca63f38a2c04d64d50
Krastyo Tanev <kr.tanev01@gmail.com>
2025-11-24 11:36:37 +0200

    Adds responsive navigation bar using MUI

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 import { useRoutes } from "react-router-dom";
 import { routes } from "./routes";
+import { Navbar } from "./components/Navbar";
+import { Box } from "@mui/material";
 import "./App.css";
 
 function App() {
   const element = useRoutes(routes);
 
-  return element;
+  return (
+    <>
+      <Navbar />
+      <Box sx={{ marginTop: "64px" }}>{element}</Box>
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,33 @@
+import { AppBar, Toolbar, Button, Box } from "@mui/material";
+import { Link as RouterLink } from "react-router-dom";
+import HomeIcon from "@mui/icons-material/Home";
+import PeopleIcon from "@mui/icons-material/People";
+
+export const Navbar = () => {
+  return (
+    <AppBar position="fixed">
+      <Toolbar>
+        <Box sx={{ display: "flex", gap: 2 }}>
+          <Button
+            color="inherit"
+            startIcon={<HomeIcon />}
+            component={RouterLink}
+            to="/"
+            sx={{ textTransform: "none", fontSize: "1rem" }}
+          >
+            Home
+          </Button>
+          <Button
+            color="inherit"
+            startIcon={<PeopleIcon />}
+            component={RouterLink}
+            to="/users"
+            sx={{ textTransform: "none", fontSize: "1rem" }}
+          >
+            Users
+          </Button>
+        </Box>
+      </Toolbar>
+    </AppBar>
+  );
+};
